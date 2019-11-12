eaf61fc5c91c608e413b7230c5032525c7b6919e
Lukas Panni <lukas.panni@outlook.de>
2019-11-12 17:03:38 +0100

    Added Heart Rate and Battery

--- app/index.js
+++ app/index.js
@@ -1,14 +1,22 @@
 import clock from "clock";
 import document from "document";
 import { preferences } from "user-settings";
+import { HeartRateSensor } from "heart-rate";
+import { battery } from "power";
 import * as util from "../common/utils";
 
 // Update the clock every minute
 clock.granularity = "seconds";
 
 // Get a handle on the <text> element
-const hm = document.getElementById("hm");
-const s = document.getElementById("s");
+const hmElem = document.getElementById("hoursMinutesText");
+const sElem = document.getElementById("secondsText");
+const hrElem = document.getElementById("heartRateText");
+const bElem = document.getElementById("batteryText");
+
+//HeartRateSensor
+const hrs = new HeartRateSensor();
+hrs.start();
 
 // Update the <text> element every tick with the current time
 clock.ontick = (evt) => {
@@ -23,6 +31,15 @@ clock.ontick = (evt) => {
   }
   let mins = util.monoDigits(today.getMinutes());
   let secs = util.monoDigits(today.getSeconds());
-  hm.text = hours + ':' + mins;
-  s.text = secs;  
+  hmElem.text = hours + ':' + mins;
+  sElem.text = secs;  
+  hrElem.text = hrs.heartRate + "bpm";
+  bElem.text =  battery.chargeLevel + "%";
+  if(battery.chargeLevel >= 75){
+    bElem.style.fill = "lime";
+  }else if(battery.chargeLevel >= 35){
+    bElem.style.fill = "yellow";
+  }else{
+    bElem.style.fill = "red";
+  }
 }
